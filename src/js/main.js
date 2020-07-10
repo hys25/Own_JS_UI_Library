e86a9719443b134d27eed918615159427e79d84c
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-07-10 16:35:17 +0300

    styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
 import $ from './lib/lib';
 
-$('button').on('click', function() {
-          $(this).toggleClass('active');
+$('#first').on('click', () => {
+    $('div').eq(1).fadeOut(800);
 });
 
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeOut(800);
+});
 
-$('button').fadeIn(1800);
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeOut(800);
+});
