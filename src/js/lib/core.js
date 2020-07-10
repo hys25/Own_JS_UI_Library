b8cbf0656658d2d8ad78ebc9feadcc26394eb4db
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-07-10 14:18:38 +0300

    methods

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -1,25 +1,25 @@
 const $ = function(selector) {
-          return new $.prototype.init(selector);
-      };
-      
-      $.prototype.init = function(selector) {
-          if (!selector) {
-              return this; // {}
-          }
+    return new $.prototype.init(selector);
+};
 
-          if (selector.tagName) {
-              this[0] = selector;
-              this.length = 1;
-              return this;
-          }
+$.prototype.init = function(selector) {
+    if (!selector) {
+        return this; // {}
+    }
 
-          Object.assign(this, document.querySelectorAll(selector));
-          this.length = document.querySelectorAll(selector).length;
-          return this;
-      };
-      
-      $.prototype.init.prototype = $.prototype;
-      
-      window.$ = $;
-      
-      export default $;
+    if (selector.tagName) {
+        this[0] = selector;
+        this.length = 1;
+        return this;
+    }
+
+    Object.assign(this, document.querySelectorAll(selector));
+    this.length = document.querySelectorAll(selector).length;
+    return this;
+};
+
+$.prototype.init.prototype = $.prototype;
+
+window.$ = $;
+
+export default $;
