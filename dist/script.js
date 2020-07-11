c20d61a7b697d3ce899aef5a0a756096a9f7557d
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-07-11 09:26:14 +0300

    cards, modal

--- dist/script.js
+++ dist/script.js
@@ -1892,6 +1892,47 @@ Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.dropdown-toggle').dropdo
 
 /***/ }),
 
+/***/ "./src/js/lib/components/modal.js":
+/*!****************************************!*\
+  !*** ./src/js/lib/components/modal.js ***!
+  \****************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function () {
+  for (let i = 0; i < this.length; i++) {
+    const target = this[i].getAttribute('data-target');
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).click(e => {
+      e.preventDefault();
+      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).fadeIn(500);
+      document.body.style.overflow = 'hiden';
+    });
+  }
+
+  const closeElements = document.querySelectorAll('[data-close]');
+  closeElements.forEach(elem => {
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(elem).click(() => {
+      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').fadeOut(500);
+      document.body.style.overflow = '';
+    });
+  });
+  Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').click(e => {
+    if (e.target.classList.contains('modal')) {
+      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').fadeOut(500);
+      document.body.style.overflow = '';
+    }
+  });
+};
+
+Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-toggle="modal"]').modal();
+
+/***/ }),
+
 /***/ "./src/js/lib/core.js":
 /*!****************************!*\
   !*** ./src/js/lib/core.js ***!
@@ -1943,6 +1984,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_actions__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/actions */ "./src/js/lib/modules/actions.js");
 /* harmony import */ var _modules_effects__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/effects */ "./src/js/lib/modules/effects.js");
 /* harmony import */ var _components_dropdown__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./components/dropdown */ "./src/js/lib/components/dropdown.js");
+/* harmony import */ var _components_modal__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/modal */ "./src/js/lib/components/modal.js");
+
 
 
 
@@ -2347,6 +2390,16 @@ Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-count="second"]'
 Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('button').eq(2).on('click', () => {
   Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.w-500').fadeToggle(800);
 });
+Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('wrap').html(`
+    <div class="dropdown">
+    <buttoniv class="btn btn-primery dropdown-toggle" id="dropdowmMenuButton">Dropdown button</buttoniv>
+    <div class="dropdown-menu" data-toggle-id="dropdowmMenuButton">
+        <a href="#" class="dropdown-item">Action1</a>
+        <a href="#" class="dropdown-item">Action2</a>
+        <a href="#" class="dropdown-item">Action3</a>
+    </div>
+    </div>`);
+Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.dropdown-toggle').dropdown();
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,17 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
+
+$('wrap').html (
+    `
+    <div class="dropdown">
+    <buttoniv class="btn btn-primery dropdown-toggle" id="dropdowmMenuButton">Dropdown button</buttoniv>
+    <div class="dropdown-menu" data-toggle-id="dropdowmMenuButton">
+        <a href="#" class="dropdown-item">Action1</a>
+        <a href="#" class="dropdown-item">Action2</a>
+        <a href="#" class="dropdown-item">Action3</a>
+    </div>
+    </div>`
+);
+
+$('.dropdown-toggle').dropdown();
