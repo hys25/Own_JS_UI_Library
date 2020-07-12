ee1c83387f4624dbf082b6630b1477684a97c6ce
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-07-12 11:20:18 +0300

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -12,16 +12,35 @@ $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
 
-$('wrap').html (
-    `
-    <div class="dropdown">
-    <buttoniv class="btn btn-primery dropdown-toggle" id="dropdowmMenuButton">Dropdown button</buttoniv>
-    <div class="dropdown-menu" data-toggle-id="dropdowmMenuButton">
-        <a href="#" class="dropdown-item">Action1</a>
-        <a href="#" class="dropdown-item">Action2</a>
-        <a href="#" class="dropdown-item">Action3</a>
-    </div>
-    </div>`
-);
-
-$('.dropdown-toggle').dropdown();
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quam corporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit veritatis, suscipit voluptatibus!'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ],
+            [
+                'Another btn',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('Hello World');
+                }
+            ]
+        ]
+    }
+}));
